e6100b9050764436eb49df74d545c82017b24008
moushumiseal <moushumiseal@gmail.com>
2019-11-09 17:01:01 +0800

    Making changes to AppComponent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TodoDataService} from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  newTodo: Todo = new Todo();
+  constructor(private todoDataService: TodoDataService){
+  }
+
+  toggleTodoComplete(todo: Todo){
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  remove(todo: Todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  getTodos(){
+    return this.todoDataService.getAllTodos();
+  }
+
 }
